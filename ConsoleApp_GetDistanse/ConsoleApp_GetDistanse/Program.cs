090fc2aa630f6eb821cb063d6d7a61d716e0d763
SergeyMNet <sergeym.net@gmail.com>
2016-06-13 21:47:07 +0300

    Create new

---
+++ ConsoleApp_GetDistanse/ConsoleApp_GetDistanse/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_GetDistanse
+{
+    class Program
+    {
+        // Distance km
+        static void Main(string[] args)
+        {
+            double lat1 = 51.491594;
+            double lon1 = 31.298318;
+
+            double lat2 = 51.493935;
+            double lon2 = 31.294944;
+
+            double dist = (6371 *
+                Math.Acos(Math.Cos(DegreeToRadian(lat1)) *
+                Math.Cos(DegreeToRadian(lat2)) *
+                Math.Cos(DegreeToRadian(lon2) - DegreeToRadian(lon1)) +
+                Math.Sin(DegreeToRadian(lat1)) *
+                Math.Sin(DegreeToRadian(lat2))));
+
+            //(350m)
+            Console.WriteLine("-----------dist km = " + dist);
+            Console.ReadKey();
+        }
+
+
+        private static double DegreeToRadian(double angle)
+        {
+            return Math.PI * angle / 180.0;
+        }
+    }
+}
